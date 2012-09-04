d0a371120189fbd0e2248539f7bd0b495d8c382b
Joshua Ruebusch <ruebusjw@muohio.edu>
2012-09-04 09:40:14 -0400

    Added Gradient, Mouse Interaction and Animation
    Very debatable if these are "interesting"

--- src/HW01_ruebusjwApp.cpp
+++ src/HW01_ruebusjwApp.cpp
@@ -22,9 +22,15 @@ private:
 	
 	int frameNumber;
 	int selector;
+	int startx1;
+	int endx1;
+	int starty1;
+	int endy1;
+	int startx2, starty2, endx2, endy2;
+	bool down;
 
 
-	static const int appWidth = 1024;
+	static const int appWidth = 800;
 	static const int appHeight = 600;
 	static const int appTextureSize = 1024;
 
@@ -34,7 +40,7 @@ private:
 
 	void drawLine(uint8_t pixels, int x1, int x2, int y1, int y2, Color8u fill);
 
-	void drawGradient (uint8_t pixels, Color8u startColor, Color8u endColor);
+	//void drawGradient(uint8_t pixels, Color8u startColor, Color8u endColor);
 	
 
 	void HW01_ruebusjwApp::prepareSettings(Settings* settings)
@@ -80,23 +86,56 @@ private:
 					pixelCount = 0;
 					isPartOfRect = false;
 				}
-
 			}
-	
-			
+		}
+	}
+
+	void HW01_ruebusjwApp::drawGradient(uint8_t* pixels, Color8u startColor, Color8u endColor)
+	{
+		int count = 0;
+		for(int i = 0; i < appTextureSize*appTextureSize; i++)
+		{
+		    if(count>=appTextureSize)
+			{
+				startColor.g = 255;
+				count = 0;
+			}
+
+			pixels[3*i] = startColor.r;
+			pixels[3*i+1] = startColor.g;
+			pixels[3*i+2] = startColor.b;
+			count++;
+
+			if(count%3 == 0)
+			{
+				startColor.g--;
+			}
+
 		}
 
-
-
-		
 	}
+
 	
-	
+
 };
+
+
 
 void HW01_ruebusjwApp::setup()
 {
 	frameNumber = 0;
+	startx1 = 0;
+	endx1 = 800;
+	starty1 = 120;
+	endy1 = 140;
+
+	startx2 = 0;
+	endx2 = 800;
+	starty2 = 90;
+	endy2 = 70;
+
+	down=false;
+
 	mySurface = new Surface(appTextureSize,appTextureSize,false);
 
 	uint8_t* dataArray = (*mySurface).getData();
@@ -104,47 +143,61 @@ void HW01_ruebusjwApp::setup()
 
 	for(int i=0; i < (*mySurface).getWidth()*(*mySurface).getHeight(); i++)
 		{
-			selector = rand() % 1000 + 1;
-			if(selector == 1)
 			{
 				dataArray[3*i] = 255;
 				dataArray[3*i+1] = 255;
 				dataArray[3*i+2] = 255;
 			}
-			else
-			{
-				dataArray[3*i] = 0;
-				dataArray[3*i+1] = 0;
-				dataArray[3*i+2] = 0;
-			}
-			
 		}
 }
 
 void HW01_ruebusjwApp::mouseDown( MouseEvent event )
 {
+	Color8u startGrad = Color8u(0,0,0);
+	Color8u endGrad = Color8u(0,255,0);
+	uint8_t* dataArray = (*mySurface).getData();
+
+	if(event.isLeftDown())
+		down = true;
+	if(event.isLeftDown()==false)
+		down = false;
 }
 
 void HW01_ruebusjwApp::update()
 {
-	int startx = 1;
-	int endx = 75;
-	int starty = 1;
-	int endy = 25;
+	
 	uint8_t* dataArray = (*mySurface).getData();
-	Color8u fill = Color8u(255,0,0);
-	drawRectangle(dataArray,startx,endx,starty,endy,fill);
-	/*
-	startx++;
-	endx++;
-	starty++;
-	endy++;*/
-	/*
-	for(int i=0; i< (*mySurface).getWidth()*(*mySurface).getHeight(); i++)
-	{
+	Color8u fill = Color8u(0,0,255);
+	Color8u fill2 = Color8u(255,0,0);
 
+	Color8u startGrad = Color8u(0,0,0);
+	Color8u endGrad = Color8u(0,255,0);
+
+	
+	
+	if(down)
+	{
+		drawGradient(dataArray,startGrad,endGrad);
 	}
-	*/
+	else
+	{
+		for(int i=0; i < (*mySurface).getWidth()*(*mySurface).getHeight(); i++)
+		{
+			{
+				dataArray[3*i] = 255;
+				dataArray[3*i+1] = 255;
+				dataArray[3*i+2] = 255;
+			}
+		}
+	}
+
+	
+	startx1++;
+	endx2=endx2-3;
+
+	drawRectangle(dataArray,startx1,endx1,starty1,endy1,fill);
+	drawRectangle(dataArray,startx2,endx2,starty2,endy2,fill2);
+	
 	frameNumber++;
 	
 }
